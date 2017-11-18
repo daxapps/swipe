6e688fd71ed850ed90fa25ac36a1dd7a911af4a4
Jason Crawford <daxapps777@gmail.com>
2017-11-17 21:55:29 -0600

    Advance deck after top card removed

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+	static defaultProps = {
+		onSwipeRight: () => {},
+		onSwipeLeft: () => {}
+	};
+
 	constructor(props) {
 		super(props);
 
@@ -17,24 +22,33 @@ class Deck extends Component {
 			},
 			onPanResponderRelease: (event, gesture) => {
 				if (gesture.dx > SWIPE_THRESHOLD) {
-					this.forceSwipe('right');
+					this.forceSwipe("right");
 				} else if (gesture.dx < -SWIPE_THRESHOLD) {
-					this.forceSwipe('left');
+					this.forceSwipe("left");
 				} else {
 					this.resetPosition();
 				}
 			}
 		});
 
-		this.state = { panResponder, position };
+		this.state = { panResponder, position, index: 0 };
 	}
 
 	forceSwipe(direction) {
-		const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+		const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
 		Animated.timing(this.state.position, {
-			toValue: { x, y: 0},
+			toValue: { x, y: 0 },
 			duration: SWIPE_OUT_DURATION
-		}).start();
+		}).start(() => this.onSwipeComplete(direction));
+	}
+
+	onSwipeComplete(direction) {
+		const { onSwipeLeft, onSwipeRight, data } = this.props;
+		const item = data[this.state.index];
+
+		direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
+		this.state.position.setValue({ x: 0, y: 0 });
+		this.setState({ index: this.state.index + 1 });
 	}
 
 	resetPosition() {
@@ -57,8 +71,12 @@ class Deck extends Component {
 	}
 
 	renderCards() {
-		return this.props.data.map((item, index) => {
-			if (index === 0) {
+		return this.props.data.map((item, i) => {
+			if (i < this.state.index) {
+				return null;
+			}
+
+			if (i === this.state.index) {
 				return (
 					<Animated.View
 						key={item.id}
