99bbe5437e3d1a893ca8da29d2c0b36c945bc92a
Jason Crawford <daxapps777@gmail.com>
2017-11-19 15:24:21 -0600

    Stack cards on top of each other

--- src/Deck.js
+++ src/Deck.js
@@ -74,7 +74,7 @@ class Deck extends Component {
 		if (this.state.index >= this.props.data.length){
 			return this.props.renderNoMoreCards();
 		}
-		
+
 		return this.props.data.map((item, i) => {
 			if (i < this.state.index) {
 				return null;
@@ -84,20 +84,32 @@ class Deck extends Component {
 				return (
 					<Animated.View
 						key={item.id}
-						style={this.getCardStyle()}
+						style={[this.getCardStyle(), styles.cardStyle]}
 						{...this.state.panResponder.panHandlers}
 					>
 						{this.props.renderCard(item)}
 					</Animated.View>
 				);
 			}
-			return this.props.renderCard(item);
-		});
+
+			return (
+				<Animated.View key={item.id} style={styles.cardStyle}>
+					{this.props.renderCard(item)}
+				</Animated.View>
+			);
+		}).reverse();
 	}
 
 	render() {
-		return <View>{this.renderCards()}</View>;
-	}
+		return <View>{this.renderCards()}</View>
+	};
 }
+
+const styles = {
+	cardStyle: {
+		position: 'absolute',
+		width: SCREEN_WIDTH
+	}
+};
 
 export default Deck;
